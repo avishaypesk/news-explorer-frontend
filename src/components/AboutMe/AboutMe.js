c6d50f1f93d24defbbd9628f7d6866778d4cc087
Avishay <avishaypesk@gmail.com>
2023-05-11 16:30:23 +0300

    feat : footer component

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,16 @@
+import './AboutMe.css';
+import profileImage from '';
+
+const AboutMe = () => {
+    return (
+        <section className="about-me">
+            <img className="about-me__image" src={profileImage} alt="Avishay Peskin"></img>
+            <div className="about-me__info-warpper">
+                <h2 className="about-me__title">About the author</h2>
+                <p className="about-me__info">{"Hi, I'm Avishay Peskin, a full-stack web developer focused on React & Node. This is the is the final project at the Pracicum course."}</p>
+            </div>
+        </section>
+    );
+};
+
+export default AboutMe;
